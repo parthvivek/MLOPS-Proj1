e8050ecc4d1745023673a41c8a9bf875220ef7be
parthvivek <kumarvivekgupta1810@gmail.com>
2025-12-06 22:31:53 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vivek Gupta",
+    author_email="kumarvivekgupta1810@gmai.com",
+    packages=find_packages()
+)
